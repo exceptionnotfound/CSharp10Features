619426da2386ea6bd297f30d90dcc90d0a46a18a
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2021-07-28 08:54:21 -0700

    Added File-Level Namespaces example

---
+++ CSharp10Demo/FileLevelNamespaces.cs
@@ -0,0 +1,13 @@
+﻿//NOTE: This will not compile at the moment, since file-level namespaces is not included
+//in the C# preview at publishing time.
+//However, this file will give you an idea of how this feature might work when C# 10 is available.
+
+namespace CSharp10Demo;
+
+public class MyClass
+{
+    public void MyMethod()
+    {
+        //...Method implementation
+    }
+}
--- CSharp10Demo/GlobalUsings.cs
+++ CSharp10Demo/GlobalUsings.cs
@@ -2,3 +2,5 @@
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Threading.Tasks;
+
+//These using statements will now be included in every C# file in this project.
